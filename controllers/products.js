afcdcc44f977c37710d36ff93fa5782278cc9518
Markson17 <marksonfrosh@gmail.com>
2023-08-09 23:09:33 +0100

    updated the controllers and routes

--- controllers/products.js
+++ controllers/products.js
@@ -1,16 +1,37 @@
 const Product = require('../models/Product');
 
-const getAllProductStatic = async (req, res) => {
-    const products = await Product.find({ featured: true})
+const getAllProductsStatic = async (req, res) => {
+    const search = 'ab'
+    const products = await Product.find({name : { $regex: search, $options: 'i'}, })
     res.status(200).json({products, nbHits: products.length })
 }
 
 const getAllProducts = async (req, res) => {
-    const products = await Product.find(req.query)
+    const { featured, company, name, sort } = req.query
+    const queryObject = {}
+    if (featured){
+        queryObject.featured = featured === 'true'? true : false
+    }
+    if (company){
+        queryObject.company = company
+    }
+    if (name){
+        queryObject.name = { $regex: name, $options: 'i'}
+    }
+    // console.log(queryObject) 
+    let result = Product.find(queryObject)
+    if (sort){
+        const sortList = sort.split(',').join(' ');
+        result = result.sort(sortList)
+    }
+    else{
+        result = result.sort('createdAt')
+    }
+    const products = await result  
     res.status(200).json({products, nbHits: products.length })
 }
 
 module.exports = {
-    getAllProductStatic,
+    getAllProductsStatic,
     getAllProducts,
 }
--- routes/products.js
+++ routes/products.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const {getAllProductStatic, getAllProducts} = require('../controllers/products')
+const {getAllProductsStatic, getAllProducts} = require('../controllers/products')
 
 router.route('/').get(getAllProducts)
-router.route('/static').get(getAllProductStatic)
+router.route('/static').get(getAllProductsStatic)
 
 module.exports = router
